3be7f56886e1c0d9b33e6bf9a471348cd2afa22e
SCD-3 <piotr.dems@gmail.com>
2025-12-14 17:30:55 +0100

    Moving help to menu

--- main.py
+++ main.py
@@ -9,21 +9,9 @@ from lunar_coin_control import SetCoinsCommand
 if __name__ == "__main__":
     
     save_file_path = None
-    
-    class HelpCommand(Command):
-        name = "help"
-        description = "Opens help menu"
-        description_long = """help [<command>] - Opens help menu. If [<command>] is given, show detailed description about given command"""
-        
-        def __call__(self, *args) -> Any:
-            if len(args) == 0:
-                for command in main_menu.options:
-                    print(f"{command.name} - {command.description}")
-            else:
-                print(main_menu.options_names[args[0]].description_long)
+
     
     main_menu = Menu("Risk of Rain 2\nASSISTANT+",
-                     HelpCommand,
                      ExitCommand,
                      OpenArtifactCommand,
                      ListArtifactsCommand, 
