bc59e77acb209f95f1511fb34b6a8b4da7c57805
SCD-3 <piotr.dems@gmail.com>
2025-12-14 20:03:11 +0100

    Introduced help command, started command system changes

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ if __name__ == "__main__":
     save_file_path = None
 
     
-    main_menu = Menu("Risk of Rain 2\nASSISTANT+",
-                     ExitCommand,
-                     OpenArtifactCommand,
-                     ListArtifactsCommand, 
-                     SetCoinsCommand,
-                     SaveFilePathCommand,
+    main_menu = Menu("Risk of Rain 2\nASSISTANT",
+                     ExitCommand(),
+                     OpenArtifactCommand(),
+                     ListArtifactsCommand(), 
+                     SetCoinsCommand(),
+                     SaveFilePathCommand(),
                      description="Assistant to game Risk of Rain 2\nType 'help' for help")
 
     main_menu.run()
--- misc/menu.py
+++ misc/menu.py
@@ -1,23 +1,40 @@
-from typing import Any, Callable, Type
+from typing import Any, Iterable, Optional
 from pyfiglet import Figlet
-from InquirerPy import inquirer
+from InquirerPy import inquirer, get_style
+from prompt_toolkit.completion import NestedCompleter
 import re as regex
 import os
-import builtins
 
 def clear_console():
+    # return
     os.system('cls' if os.name == 'nt' else 'clear')
+
+type Completion = dict[str, Completion|None]
+
+class Command:
+    
+    name: str = ""
+    description: str = ""
+    description_long: str = ""
+    
+    def __init__(self, *args, **kwargs) -> None:
+        self.arguments: Completion = {self.name: None}
+    
+    def __call__(self, *args) -> Any:
+        pass
 
 
 
 class Menu:
     
-    def __init__(self, title: str, *options: Type["Command"], description: str = ""):
+    def __init__(self, title: str, *options: Command, description: str = ""):
         clear_console()
         
         self.title = self.__prepare_title(title)
-        self.options = options
-        self.options_names = {option.name: option for option in options}
+        self.options = options + (help_cmd := HelpCommand(self),)
+        self.options_names = {option.name: option for option in self.options}
+        # print(self.options_names)
+        help_cmd.add_arguments()
         self.description = description
         
     def __prepare_title(self, title: str) -> str:
@@ -38,30 +55,39 @@ class Menu:
     
     def prompt(self):
         res: str = inquirer.text(message="",
-                            completer={cmd: None for cmd in self.options_names}).execute()
+                            # completer={cmd.name: cmd.arguments for cmd in self.options}).execute()
+                            completer=NestedCompleter.from_nested_dict({cmd.name: cmd.arguments for cmd in self.options}),
+                            qmark='>', amark='>').execute()
         res_args = Menu.__separate_arguments(res)
-        self.options_names[res_args[0]]()(*res_args[1:])
+        self.options_names[res_args[0]](*res_args[1:])
 
     def run(self):
         clear_console()
         self.display_title()
         while True:
             self.prompt()
-
-
-
-class Command:
     
-    name: str = ""
-    description: str = ""
-    description_long: str = ""
+    
+class HelpCommand(Command):
+    
+    name = "help"
+    description = "Opens help menu"
+    description_long = """help [<command>] - Opens help menu. If [<command>] is given, show detailed description about given command"""
+    
+    def __init__(self, menu: Menu) -> None:
+        self.__menu = menu
+    
+    def add_arguments(self):
+        self.arguments = {i: None for i in self.__menu.options_names.keys()}
     
     def __call__(self, *args) -> Any:
-        pass
+        if len(args) == 0:
+            for command in self.__menu.options:
+                print(f"{command.name} - {command.description}")
+        else:
+            print(self.__menu.options_names[args[0]].description_long)
+
+
 
 class ArgumentError(Exception):
     """Invalid or missing arguments for a command."""
-
-
-if __name__ == "__main__":
-    print(input(lambda cmd: True))
