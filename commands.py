e6801e896d65701beed9c022c848cbed7fcd1108
SCD-3 <piotr.dems@gmail.com>
2026-01-02 20:30:04 +0000

    fixed bug with typehints

--- commands.py
+++ commands.py
@@ -1,8 +1,3 @@
 #from items.items_commands import OpenItemsCommand, ListItemsCommand
 from lunar_coin_control import SetCoinsCommand
 from misc.generic import SaveFileCommand
-
-def SetCoinsCommand(*args):
-    ...
-def SaveFileCommand(*args):
-    ...
--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from devmode.dev import EnableDevCommand
 
 if __name__ == "__main__":
     
-    save_file_path = None
-
-    
     main_menu = Menu("Risk of Rain 2\nASSISTANT",
                      "Assistant to game [#C4CF4E]Risk of Rain 2",
                      #OpenItemsCommand(),
