21c7900ffc02889e8019d270860e7fa8bbb2942c
SCD-3 <piotr.dems@gmail.com>
2026-01-04 15:57:25 +0100

    improved a bit devmenu command

--- devmode/devstart.py
+++ devmode/devstart.py
@@ -1,4 +1,4 @@
-from misc.menu import Command, Completion, Menu
+from misc.menu import Command, Completion, Menu, ArgumentError
 from devmode.devmenu import dev_menu
 
 class EnableDevCommand(Command):
@@ -9,7 +9,12 @@ class EnableDevCommand(Command):
         self.menu = menu
     
     def run(self, *args):
-        FLAG_RUN_AS_MAIN = len(args) >= 1 and args[0] == "asmain"
+        FLAG_RUN_AS_MAIN = False
+        if len(args) >= 1:
+            if args[0] == "asmain":
+                FLAG_RUN_AS_MAIN = True
+            else:
+                raise ArgumentError("Invalid argument given. Accepts only 'asmain'.")
         
         if FLAG_RUN_AS_MAIN:
             dev_menu.run_main()
