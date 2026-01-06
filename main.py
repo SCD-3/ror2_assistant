8de6512caa3fb7e6b0e1e085af54a550ed60854b
SCD-3 <piotr.dems@gmail.com>
2026-01-06 18:55:09 +0100

    removed useless indentation

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from devmode.devstart import EnableDevCommand
 if __name__ == "__main__":
     
     main_menu = Menu("Risk of Rain 2\nASSISTANT",
-                     "Assistant to game [#C4CF4E]Risk of Rain 2")
+                    "Assistant to game [#C4CF4E]Risk of Rain 2")
     
     main_menu.options = (
         SetCoinsCommand(),
         SaveFileCommand(),
         EnableDevCommand()
         )
-                     
+
 
     main_menu.run_main()
