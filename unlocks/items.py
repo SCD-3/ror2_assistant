ad783588366c20ebdd21a390672234221b4cc5aa
SCD-3 <piotr.dems@gmail.com>
2026-01-14 21:36:52 +0100

    Item class

--- unlocks/items.py
+++ unlocks/items.py
@@ -1,2 +1,21 @@
+import misc.enums
+
 class Item:
-    pass #TODO
+
+    def __init__(self,
+                name: str,
+                desc: str,
+                desc_long: str,
+                *, 
+                scaling: dict[str, tuple[int|float, misc.enums.Stacking]],
+                rarity: tuple[misc.enums.Rarity, ...],
+                corrupts: 'None|Item' = None,
+                hardcap_at: None|int = None) -> None:
+        
+        self.name = name
+        self.desc = desc
+        self.desc_long = desc_long
+        self.scaling = scaling
+        self.rarity = rarity
+        self.corrupts = corrupts
+        self.hardcap_at = hardcap_at
