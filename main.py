dfa6d560bf157eeecc0d10cf10169d5e12619123
SCD-3 <piotr.dems@gmail.com>
2026-01-17 12:51:49 +0100

    begone catche

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from misc.menu import Menu
-from commands import *
+import webbrowser
 
+from misc.menu import Menu, Command
+from output import console
+from commands import *
 from devmode.devstart import EnableDevCommand
+
+
+class LaunchGameCommand(Command):
+    
+    name = 'rungame'
+    description = "Run game [#C4CF4E]Risk of Rain 2"
+    
+    def run(self, *args):
+        console.print("Launch game via a [blue]Steam[/blue]...")
+        webbrowser.open(r'steam://rungameid/632360')
 
 
 if __name__ == "__main__":
     
     main_menu = Menu("Risk of Rain 2\nASSISTANT",
-                    "Assistant to game [#C4CF4E]Risk of Rain 2")
-    
-    main_menu.options = (
-        SetCoinsCommand(),
-        OpenConfigCommand(),
-        EnableDevCommand()
-        )
+                    "Assistant to game [#C4CF4E]Risk of Rain 2",
+                    LaunchGameCommand(),
+                    SetCoinsCommand(),
+                    OpenConfigCommand(),
+                    EnableDevCommand())
 
     main_menu.run_main()
